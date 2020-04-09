6f140f73f648cc29b2c0614630a184569470d510
Matt Leuer <mattleuer@gmail.com>
2020-04-09 15:58:37 -0700

    getUser returns github profile of specified user.

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-const questions = [
+const api = require("./utils/api.js");
 
+
+const questions = [
+    {
+        type: "input",
+        name: "githubUsername",
+        message: "What is your Github username?"
+    },
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the title of your project?"
+    },
+    {
+        type: "input",
+        name: "projectDescription",
+        message: "Please describe your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use."
+    },
+    {
+        type: "rawlist",
+        name: "license",
+        message: "Choose your license",
+        choices: ["ISC License (People can do anything with your code with proper attribution)", "MIT License (Simple and Permissive)"]
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Comma separated list of Github profiles of contributors"
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Comma separated list of Github profiles of contributors"
+    }
 ];
 
 function writeToFile(fileName, data) {
--- utils/api.js
+++ utils/api.js
@@ -1,6 +1,12 @@
-const api = {
-  getUser(username) {
+const axios = require("axios");
 
+const api = {
+  getUser: function(username) {
+    const accessToken = "3c6c2dfb3a3b8c8e741d31f9796e78aedc7625e6";
+    axios.get(`https://api.github.com/users/${username}?access_token=${accessToken}`)
+    .then(function(response) {
+      console.log(response.data);
+    })
   }
 };
 
